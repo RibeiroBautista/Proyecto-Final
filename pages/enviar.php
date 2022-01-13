50682252727195d1200e3a5ba0d7fea7666b75dc
Ribeiro Bautista <bautyelrey@hotmail.com>
2022-01-13 15:46:03 -0300

    proyecto-final-13/1-15:45hs

--- pages/enviar.php
+++ pages/enviar.php
@@ -10,17 +10,16 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por: " . $name $apellido . " \r\n";
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'bautyelrey@hotmail.com.com';
+$para = 'bautyelrey@hotmail.com';
 $asunto = 'Pruebas del Formulario';
 
 mail($para, $asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
-?>
 ?>
